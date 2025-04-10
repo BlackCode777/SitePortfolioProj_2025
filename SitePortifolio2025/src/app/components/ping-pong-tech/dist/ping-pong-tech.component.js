a61949d00d0fcac1657225a25274a58579b5cbdd
Anderson Martins <BlackCode77.1980@gmail.com>
2025-04-10 18:11:08 -0300

    Data 10/04/2025 - Acrecentando pasman comedor de tecnologia.

--- SitePortifolio2025/src/app/components/ping-pong-tech/dist/ping-pong-tech.component.js
+++ SitePortifolio2025/src/app/components/ping-pong-tech/dist/ping-pong-tech.component.js
@@ -7,11 +7,63 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 exports.__esModule = true;
 exports.PingPongTechComponent = void 0;
-var core_1 = require("@angular/core");
 var common_1 = require("@angular/common");
+var core_1 = require("@angular/core");
 var PingPongTechComponent = /** @class */ (function () {
-    function PingPongTechComponent() {
+    function PingPongTechComponent(renderer, elRef) {
+        this.renderer = renderer;
+        this.elRef = elRef;
     }
+    PingPongTechComponent.prototype.ngAfterViewInit = function () {
+        this.startPacmanAnimation();
+    };
+    PingPongTechComponent.prototype.startPacmanAnimation = function () {
+        var _this = this;
+        var pacman = this.elRef.nativeElement.querySelector('#pacman');
+        var logos = Array.from(this.elRef.nativeElement.querySelectorAll('.logo'));
+        var speed = 2; // Velocidade do Pacman
+        var pacmanWidth = pacman.offsetWidth;
+        var containerWidth = this.elRef.nativeElement.querySelector('.bouncer-container').offsetWidth;
+        var positionX = 0;
+        var direction = 1; // 1: indo, -1: voltando
+        var resetLogos = function () {
+            logos.forEach(function (logo) {
+                logo.classList.remove('eaten');
+                _this.renderer.setStyle(logo, 'opacity', '1');
+            });
+        };
+        var movePacman = function () {
+            positionX += speed * direction;
+            pacman.style.left = positionX + "px";
+            pacman.style.transform = direction === 1 ? 'scaleX(1)' : 'scaleX(-1)'; // vira o pacman
+            logos.forEach(function (logo) {
+                if (!logo.classList.contains('eaten')) {
+                    var pacmanRect = pacman.getBoundingClientRect();
+                    var logoRect = logo.getBoundingClientRect();
+                    var collided = pacmanRect.right >= logoRect.left &&
+                        pacmanRect.left <= logoRect.right &&
+                        pacmanRect.top <= logoRect.bottom &&
+                        pacmanRect.bottom >= logoRect.top;
+                    if (collided) {
+                        logo.classList.add('eaten');
+                        _this.renderer.setStyle(logo, 'transition', 'opacity 0.4s ease-in-out');
+                        _this.renderer.setStyle(logo, 'opacity', '0');
+                    }
+                }
+            });
+            var reachedRightEnd = direction === 1 && (positionX + pacmanWidth >= containerWidth);
+            var reachedLeftEnd = direction === -1 && positionX <= 0;
+            if (reachedRightEnd) {
+                direction = -1; // muda direção para voltar
+            }
+            else if (reachedLeftEnd) {
+                direction = 1; // muda direção para ir
+                resetLogos(); // bolinhas reaparecem na nova ida
+            }
+            requestAnimationFrame(movePacman);
+        };
+        requestAnimationFrame(movePacman);
+    };
     PingPongTechComponent = __decorate([
         core_1.Component({
             selector: 'app-ping-pong-tech',
--- SitePortifolio2025/src/app/components/ping-pong-tech/ping-pong-tech.component.ts
+++ SitePortifolio2025/src/app/components/ping-pong-tech/ping-pong-tech.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-ping-pong-tech',
@@ -10,7 +10,69 @@ import { CommonModule } from '@angular/common';
   templateUrl: './ping-pong-tech.component.html',
   styleUrl: './ping-pong-tech.component.css'
 })
-export class PingPongTechComponent {
+export class PingPongTechComponent implements AfterViewInit {
 
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
+
+  ngAfterViewInit(): void {
+    this.startPacmanAnimation();
+  }
+
+  startPacmanAnimation() {
+    const pacman = this.elRef.nativeElement.querySelector('#pacman') as HTMLImageElement;
+    const logos = Array.from(this.elRef.nativeElement.querySelectorAll('.logo')) as HTMLImageElement[];
+
+    const speed = 2; // Velocidade do Pacman
+    const pacmanWidth = pacman.offsetWidth;
+    const containerWidth = this.elRef.nativeElement.querySelector('.bouncer-container').offsetWidth;
+
+    let positionX = 0;
+    let direction = 1; // 1: indo, -1: voltando
+
+    const resetLogos = () => {
+      logos.forEach(logo => {
+        logo.classList.remove('eaten');
+        this.renderer.setStyle(logo, 'opacity', '1');
+      });
+    };
+
+    const movePacman = () => {
+      positionX += speed * direction;
+      pacman.style.left = `${positionX}px`;
+      pacman.style.transform = direction === 1 ? 'scaleX(1)' : 'scaleX(-1)'; // vira o pacman
+
+      logos.forEach((logo) => {
+        if (!logo.classList.contains('eaten')) {
+          const pacmanRect = pacman.getBoundingClientRect();
+          const logoRect = logo.getBoundingClientRect();
+
+          const collided = pacmanRect.right >= logoRect.left &&
+            pacmanRect.left <= logoRect.right &&
+            pacmanRect.top <= logoRect.bottom &&
+            pacmanRect.bottom >= logoRect.top;
+
+          if (collided) {
+            logo.classList.add('eaten');
+            this.renderer.setStyle(logo, 'transition', 'opacity 0.4s ease-in-out');
+            this.renderer.setStyle(logo, 'opacity', '0');
+          }
+        }
+      });
+
+      const reachedRightEnd = direction === 1 && (positionX + pacmanWidth >= containerWidth);
+      const reachedLeftEnd = direction === -1 && positionX <= 0;
+
+      if (reachedRightEnd) {
+        direction = -1; // muda direção para voltar
+      } else if (reachedLeftEnd) {
+        direction = 1; // muda direção para ir
+        resetLogos();  // bolinhas reaparecem na nova ida
+      }
+
+      requestAnimationFrame(movePacman);
+    };
+
+    requestAnimationFrame(movePacman);
+  }
 
 }
